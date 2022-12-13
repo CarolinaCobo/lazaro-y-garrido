cabe9af5e85e3a040fac2b1ddc1ca12d25f19996
Carolina <ccobo.dev@gmail.com>
2022-12-13 11:48:36 +0000

    remove unused imports

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,12 +2,6 @@ import Image from 'next/image'
 
 import { Button } from '@/components/Button'
 import { Container } from '@/components/Container'
-import logoLaravel from '@/images/logos/laravel.svg'
-import logoMirage from '@/images/logos/mirage.svg'
-import logoStatamic from '@/images/logos/statamic.svg'
-import logoStaticKit from '@/images/logos/statickit.svg'
-import logoTransistor from '@/images/logos/transistor.svg'
-import logoTuple from '@/images/logos/tuple.svg'
 
 export function Hero() {
   return (
