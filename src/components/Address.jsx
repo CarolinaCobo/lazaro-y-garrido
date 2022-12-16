ae5bf9a6b84b6ad6e521370bdd3bbc742960c7c4
Carolina <ccobo.dev@gmail.com>
2022-12-16 14:51:52 +0000

    map responsive

--- src/components/Address.jsx
+++ src/components/Address.jsx
@@ -6,7 +6,7 @@ export function Address() {
       <h2 className="mt-20 text-3xl font-bold tracking-tight text-gray-900">
         Dirección
       </h2>
-      <div className="mt-8 flex gap-1 sm:grid-cols-2 md:grid md:grid-cols-2 lg:grid-cols-2">
+      <div className="mt-8 gap-1 sm:grid-cols-2 md:flex lg:grid  lg:grid-cols-2">
         <div>
           <Link
             href="https://goo.gl/maps/E3abA2DAn8Ny2h7D6"
@@ -17,14 +17,14 @@ export function Address() {
           </Link>
           <h3 className="text-lg font-medium text-gray-900"></h3>
         </div>
-        <div className="hidden sm:contents lg:relative lg:block lg:flex-1">
+        <div className=" sm:contents lg:relative lg:block lg:flex-1">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15675.304856498467!2d2.651404477143287!3d39.56998197884075!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xfe1bb27cbd3a8cba!2sL%C3%A1zaro%20%26%20Garrido%20Abogados!5e0!3m2!1sen!2sie!4v1671098634436!5m2!1sen!2sie"
-            width="400"
-            height="300"
+            width="100%"
+            height="100%"
             allowFullScreen=""
             loading="lazy"
-            className="aspect-[4/3] w-full"
+            className="aspect-[4/3] w-full "
             referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
         </div>
