236e5735fc962448c99a433f4ae945c0f6fb468d
Carolina <ccobo.dev@gmail.com>
2022-12-16 11:57:15 +0000

    Fix import

--- src/pages/contacto.jsx
+++ src/pages/contacto.jsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { Header } from '@/components/Header'
-import { ContactPage } from '@/components/contactPage'
+import { ContactPage } from '@/components/ContactPage'
 import { Footer } from '@/components/Footer'
 
 export default function Contact() {
