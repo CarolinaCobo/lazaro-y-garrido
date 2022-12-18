dbac93cd83fc5c85f1c739377d75cbb2864f8b09
Carolina <ccobo.dev@gmail.com>
2022-12-18 09:44:09 +0000

    Add og elements

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,9 @@
 import Head from 'next/head'
-
 import { CallToAction } from '@/components/CallToAction'
 import { Press } from '@/components/Press'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { Hero } from '@/components/Hero'
-
 import { Team } from '@/components/Team'
 import { Services } from '@/components/Services'
 
@@ -14,7 +12,32 @@ export default function Home() {
     <>
       <Head>
         <title>Lázaro & Garrido</title>
-        <meta name="description" content="" />
+        <meta
+          name="description"
+          content="Abogados en Palma de Mallorca. No defendemos casos, defendemos personas."
+        />
+
+        <meta
+          key="og:title"
+          property="og:title"
+          content="Lázaro y Garrido - Abogados en Palma de Mallorca"
+        />
+        <meta
+          key="og:image"
+          property="og:image"
+          content="src/images/og/og-logo.png"
+        />
+        <meta
+          key="og:description"
+          property="og:description"
+          content="Abogados en Mallorca. No defendemos casos, defendemos personas"
+        />
+        <meta
+          key="description"
+          property="description"
+          content="Abogados en Mallorca. No defendemos casos, defendemos personas"
+        />
+        <meta property="og:url" content="http://www.lazarogarrido.com/" />
       </Head>
       <Header />
       <main>
