f871ef4458f934b43171744c4363aa41234acd81
Carolina <ccobo.dev@gmail.com>
2023-03-11 13:50:03 +0000

    Added meta tags and new image

--- src/pages/contacto.jsx
+++ src/pages/contacto.jsx
@@ -8,7 +8,33 @@ export default function Contact() {
     <>
       <Head>
         <title>Contacto - Lázaro & Garrido</title>
+        <meta
+          name="description"
+          content="Ponte en contacto con Lazaro y Garrido, abogados en Palma de Mallorca"
+        />
+        <meta
+          key="og:title"
+          property="og:title"
+          content="Contacto - Lázaro & Garrido"
+        />
+        <meta
+          key="og:image"
+          property="og:image"
+          content="src/images/og/ogimage.png"
+        />
+        <meta
+          key="og:description"
+          property="og:description"
+          content="No defendemos casos, defendemos personas. Abogados en Palma de Mallorca y todo el territorio nacional"
+        />
+        <meta
+          key="description"
+          property="description"
+          content="Abogados en Palma de Mallorca. No defendemos casos, defendemos personas."
+        />
+        <meta property="og:url" content="http://www.lazarogarrido.com/" />
       </Head>
+      <Header />
       <Header />
       <ContactPage />
       <Footer />
