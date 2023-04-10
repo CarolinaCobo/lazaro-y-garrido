8af3eeecb05f1b47ceecc8c246a0ffe53335cbd5
Carolina <ccobo.dev@gmail.com>
2023-04-10 22:55:24 +0100

    Remove duplicated

--- src/components/Press.jsx
+++ src/components/Press.jsx
@@ -6,36 +6,12 @@ const pressData = [
   [
     {
       headLine:
-        'Una juez declara nulo el despido de una madre con jornada reducida',
-      description:
-        'La empresa intentaba justificar la medida en que era la empleada con menos antigüedad',
-      href: 'https://www.ultimahora.es/noticias/local/2023/04/08/1912601/despido-baleares-declarado-nulo-madre-jornada-reducida.html',
-    },
-    {
-      headLine:
-        'Condenado un hotel tras un despedir a una trabajadora por colar a su perro en una habitación',
-      description:
-        'El tribunal ha estimado un recurso de la trabajadora, de manera que ha anulado el despido disciplinario.',
-      href: 'https://www.diariodemallorca.es/sucesos/sucesos-mallorca/2022/12/10/condenado-hotel-despedir-trabajadora-colar-79803880.html?utm_source=whatsapp&utm_medium=social&utm_campaign=btn-share',
-    },
-  ],
-  [
-    {
-      headLine:
         'Condena a un hotel de Mallorca tras un despido por colar a su perro en una habitación',
       description:
         'La trabajadora se alojó con el animal en una de las villas de lujo de la empresa, que prohíbe acceder con mascotas a sus clientes.',
       href: 'https://www.ultimahora.es/noticias/local/2022/12/10/1843939/hoteles-despido-por-meter-perro-habitacion.html',
     },
-    {
-      headLine:
-        'Condenado un hotel tras un despedir a una trabajadora por colar a su perro en una habitación',
-      description:
-        'El tribunal ha estimado un recurso de la trabajadora, de manera que ha anulado el despido disciplinario.',
-      href: 'https://www.diariodemallorca.es/sucesos/sucesos-mallorca/2022/12/10/condenado-hotel-despedir-trabajadora-colar-79803880.html?utm_source=whatsapp&utm_medium=social&utm_campaign=btn-share',
-    },
-  ],
-  [
+
     {
       headLine:
         'Imputan a una mujer por liarse a palos con unos inquilinos y coaccionar a una familia con una empresa de desokupación',
@@ -43,12 +19,21 @@ const pressData = [
         'Según denuncia el afectado, cuando la propietaria se enteró de que el hombre había vuelto a acceder a la vivienda, la mujer acudió al edificio en el que creía que vivía él, con la intención de agredirle, pero se equivocó de piso y pegó a dos personas que no tenían nada que ver.',
       href: 'https://www.eldiario.es/illes-balears/sociedad/imputan-mujer-liarse-palos-inquilinos-coaccionar-familia-empresa-desokupacion_1_9677988.html',
     },
+  ],
+  [
     {
       headLine:
         'Tres detenidos por engañar a un empleado al que hicieron testaferro de una empresa con deudas',
       description:
         'La Policía Nacional detiene en Palma a tres personas acusadas de un presunto delito contra los derechos de los trabajadores, falsedad documental y contra la seguridad social.        ',
       href: 'https://www.mallorcadiario.com/detenidos-3-empresarios-por-enganar-a-un-trabajador-que-hicieron-testaferro-de-una-empresa-con-deudas',
+    },
+    {
+      headLine:
+        'Condenado un hotel tras un despedir a una trabajadora por colar a su perro en una habitación',
+      description:
+        'El tribunal ha estimado un recurso de la trabajadora, de manera que ha anulado el despido disciplinario.',
+      href: 'https://www.diariodemallorca.es/sucesos/sucesos-mallorca/2022/12/10/condenado-hotel-despedir-trabajadora-colar-79803880.html?utm_source=whatsapp&utm_medium=social&utm_campaign=btn-share',
     },
   ],
   [
@@ -65,6 +50,15 @@ const pressData = [
       description:
         'El dueño de una empresa de alquiler de coches utilizaba a inmigrantes e indigentes como testaferros - Evitaba el pago de las cuotas de sus trabajadores, impuestos y deudas a proveedores.',
       href: 'https://www.diariodemallorca.es/sucesos/2019/09/20/diez-detenidos-palma-fraude-600-2842725.html',
+    },
+  ],
+  [
+    {
+      headLine:
+        'Una juez declara nulo el despido de una madre con jornada reducida',
+      description:
+        'La empresa intentaba justificar la medida en que era la empleada con menos antigüedad',
+      href: 'https://www.ultimahora.es/noticias/local/2023/04/08/1912601/despido-baleares-declarado-nulo-madre-jornada-reducida.html',
     },
   ],
 ]
